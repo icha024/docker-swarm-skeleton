dad3c3fd1a04a58d32c224fb81f1079580579872
Ian Chan <icha024@gmail.com>
2017-01-19 18:16:37 +1300

    Added default localhost for Portainer. Some polishing

--- populateSwarm.sh
+++ populateSwarm.sh
@@ -1,9 +1,23 @@
-docker network create monitoring -d overlay
+docker network create monitoring --opt encrypted -d overlay 
 
-## For multi-machine, setup dedicate box with storage then use a contraint. eg: --constraint node.hostname==worker1 
+## Create folder for storage, change the src name for ElasticSearch and Portainer if needed.
+## Alternatively: docker volume create --name monitoring-volume
+# mkdir -p /home/docker/ironswarm
+
+## For multi-machine, setup dedicate box with storage then use a contraint. eg: --constraint node.hostname==myhostname 
+## For storage: --mount "type=bind,src=/home/docker/ironswarm/,dst=/usr/share/elasticsearch/data,ro=false" \
 docker service create -p 9200:9200 --network=monitoring \
-  --mount type=volume,target=/usr/share/elasticsearch/data \
+  --mount "type=volume,dst=/usr/share/elasticsearch/data" \
   --name elasticsearch elasticsearch:5
+
+## For multi-machine, setup dedicate box with storage then use a contraint. eg: --constraint node.hostname==myhostname 
+## For storage: --mount "type=bind,src=/home/docker/ironswarm/,dst=/data,ro=false" \
+docker service create \
+    --name portainer \
+    --publish 9000:9000 \
+    --mount "type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock" \
+    --mount "type=volume,dst=/data" \
+    portainer/portainer --host=unix:///var/run/docker.sock
 
 docker service create --network=monitoring --name kibana -e ELASTICSEARCH_URL="http://elasticsearch:9200" -p 5601:5601 -e LOGSPOUT=ignore kibana:5
 
@@ -18,5 +32,5 @@ docker service create --network=monitoring --name logspout \
 docker service create --network=monitoring --mode global --name=metricbeat \
 --mount "type=bind,source=/sys/fs/cgroup,target=/hostfs/sys/fs/cgroup,readonly=true" metricbeat-custom
 
-## Test: docker run --rm --log-driver=gelf --log-opt gelf-address=udp://$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' logstash):12201 --log-opt tag="test" alpine /bin/sh -c "while true; do echo My Message \$RANDOM; sleep 1; done;"
+## Testing. Remove me.
 docker service create --network=monitoring --name=tester --replicas=1 alpine /bin/sh -c "while true; do echo My Message \$RANDOM; sleep 1; done;"
