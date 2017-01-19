dff7fccfbbae50f2ee21887571fdec1183fd50df
Ian Chan <icha024@gmail.com>
2017-01-19 14:08:01 +1300

    Polishing

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# Quick Docker Swarm setup
+# Iron Swarm: Opinionated Docker Swarm Platform Setup
 
-- Script to setup ELK, 
-- Forward all container's stdout/err (logspout) to ELK
-- Forwarding all systems monitoring to it (cadvisor)
+- Script to setup ELK
+- Forwards all container's stdout/err to ELK (logspout)
+- Setup systems monitoring metics on ELK (metricbeat)
+
+## Loading Kibana metric dashboard
+Docker Swarm, as at Jan 2017, does not support one-off short task. A workaround is to start a service to setup dashboard then remove the service afterwards.
+### Load sample metric dashboards
+`docker service create --network=monitoring --name=load-dashboards --replicas=1 athieriot/metricbeat /bin/sh -c "./scripts/import_dashboards -es http://elasticsearch:9200 && while true; do sleep 60; done;"`
+
+### Clean up once loaded
+`docker service rm load-dashboards`
---
+++ populateSwarm.sh
@@ -0,0 +1,22 @@
+docker network create monitoring -d overlay
+
+## For multi-machine, setup dedicate box with storage then use a contraint. eg: --constraint node.hostname==worker1 
+docker service create -p 9200:9200 --network=monitoring \
+  --mount type=volume,target=/usr/share/elasticsearch/data \
+  --name elasticsearch elasticsearch:5
+
+docker service create --network=monitoring --name kibana -e ELASTICSEARCH_URL="http://elasticsearch:9200" -p 5601:5601 kibana:5
+
+docker service create --network=monitoring --mode global --name=logstash-syslog logstash-syslog
+
+docker service create --network=monitoring --name logspout \
+    --mode global \
+    --mount "type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock" \
+    -e SYSLOG_FORMAT=rfc3164 \
+    gliderlabs/logspout syslog://logstash-syslog:5000
+
+docker service create --network=monitoring --mode global --name=metricbeat \
+--mount "type=bind,source=/sys/fs/cgroup,target=/hostfs/sys/fs/cgroup,readonly=true" metricbeat-custom
+
+## Test: docker run --rm --log-driver=gelf --log-opt gelf-address=udp://$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' logstash):12201 --log-opt tag="test" alpine /bin/sh -c "while true; do echo My Message \$RANDOM; sleep 1; done;"
+docker service create --network=monitoring --name=tester --replicas=1 alpine /bin/sh -c "while true; do echo My Message \$RANDOM; sleep 1; done;"
